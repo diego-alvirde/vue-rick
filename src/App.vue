6a4f6f6d7a919a8c106cd153a8a34e106a4e4d05
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-04-27 17:19:09 -0500

    Consultar datos y desplegarlos

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
   <div id="app">
-    <h1 class="title">Hola Mundo</h1>
+    <h1 class="title">Consultar Datos</h1>
+    <button v-on:click="fetch">Consultar</button>
+    <div v-for="character in characters" v-bind:key="character.id">
+      {{ character }}
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      characters: [],
+    };
+  },
+  methods: {
+    fetch() {
+      let result = this.$http
+        .get("https://rickandmortyapi.com/api/character")
+        .then((res) => {
+          this.characters = res.data.results;
+          console.log(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
