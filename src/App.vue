aacb25271a300a4b7214a0f4e79c1670f58ab60e
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2020-04-27 17:45:40 -0500

    Maquetación de interfaz

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <template>
   <div id="app">
-    <h1 class="title">Consultar Datos</h1>
-    <button v-on:click="fetch">Consultar</button>
-    <div v-for="character in characters" v-bind:key="character.id">
-      {{ character }}
+    <div class="hero is-white is-gradient is-bold">
+      <div class="hero-body">
+        <h1 class="title">
+          <span class="has-text-success">R&M</span>
+          <span class="subtitle">Personajes</span>
+        </h1>
+        <button v-on:click="fetch" class="button is-success is-rounded">
+          Consultar
+        </button>
+      </div>
+    </div>
+    <div class="container">
+      <div
+        class="columns is-desktop is-mobile is-tablet is-multiline is centered"
+      >
+        <div
+          class="column is-12-mobile is-4-desktop is-4-tablet"
+          v-for="character in characters"
+          v-bind:key="character.id"
+        >
+          <div class="card">
+            <div class="card-header">
+              <img :src="character.image" :alt="character.name" />
+            </div>
+            <div class="card-content">
+              <h3 class="title is-size-4">{{ character.name }}</h3>
+              <button class="button is-success is-rounded is-small">
+                Ver mas
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
